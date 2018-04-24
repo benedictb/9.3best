0ddcc61f296edc8c05ebe0b995a1e5b3781fac67
Daniel Kerrigan <dkerriga@nd.edu>
2018-04-23 21:22:09 -0400

    working on front end javascript

--- update4/example.py
+++ update4/example.py
@@ -11,7 +11,34 @@ sample_students = [
 sample_roster = [
     {'name': 'Tanya Williams', 'key': 1, 'in': True, 'out': True},
     {'name': 'Katie Smith', 'key': 2, 'in': True, 'out': False},
-    {'name': 'Bobby Hoover', 'key': 3, 'in': False, 'out': False}
+    {'name': 'Bobby Hoover', 'key': 3, 'in': False, 'out': False},
+    {'name': 'Tanya Williams', 'key': 4, 'in': True, 'out': True},
+    {'name': 'Katie Smith', 'key': 5, 'in': True, 'out': False},
+    {'name': 'Bobby Hoover', 'key': 6, 'in': False, 'out': False},
+    {'name': 'Tanya Williams', 'key': 7, 'in': True, 'out': True},
+    {'name': 'Katie Smith', 'key': 8, 'in': True, 'out': False},
+    {'name': 'Bobby Hoover', 'key': 9, 'in': False, 'out': False},
+    {'name': 'Tanya Williams', 'key': 10, 'in': True, 'out': True},
+    {'name': 'Katie Smith', 'key': 11, 'in': True, 'out': False},
+    {'name': 'Bobby Hoover', 'key': 12, 'in': False, 'out': False},
+    {'name': 'Tanya Williams', 'key': 13, 'in': True, 'out': True},
+    {'name': 'Katie Smith', 'key': 14, 'in': True, 'out': False},
+    {'name': 'Bobby Hoover', 'key': 15, 'in': False, 'out': False},
+    {'name': 'Tanya Williams', 'key': 16, 'in': True, 'out': True},
+    {'name': 'Katie Smith', 'key': 17, 'in': True, 'out': False},
+    {'name': 'Bobby Hoover', 'key': 18, 'in': False, 'out': False},
+    {'name': 'Tanya Williams', 'key': 19, 'in': True, 'out': True},
+    {'name': 'Katie Smith', 'key': 20, 'in': True, 'out': False},
+    {'name': 'Bobby Hoover', 'key': 21, 'in': False, 'out': False},
+    {'name': 'Tanya Williams', 'key': 22, 'in': True, 'out': True},
+    {'name': 'Katie Smith', 'key': 23, 'in': True, 'out': False},
+    {'name': 'Bobby Hoover', 'key': 24, 'in': False, 'out': False},
+    {'name': 'Tanya Williams', 'key': 25, 'in': True, 'out': True},
+    {'name': 'Katie Smith', 'key': 26, 'in': True, 'out': False},
+    {'name': 'Bobby Hoover', 'key': 27, 'in': False, 'out': False},
+    {'name': 'Tanya Williams', 'key': 28, 'in': True, 'out': True},
+    {'name': 'Katie Smith', 'key': 29, 'in': True, 'out': False},
+    {'name': 'Bobby Hoover', 'key': 30, 'in': False, 'out': False},
 ]
 
 sample_classes = [
@@ -22,16 +49,18 @@ sample_classes = [
     {'name': 'Tutoring', 'key': 5}
 ]
 
+
 @app.route('/', methods=['GET'])
 def landing():
     if request.method == 'GET':
         return render_template('landing.html')
 
+
 @app.route('/classes', methods=['GET'])
 def classes():
     if request.method == 'GET':
         date = request.args.get('date', '')
-        if date == '2018-04-17':
+        if date == '2018-04-18':
             return jsonify(sample_classes)
         else:
             return jsonify(sample_classes[::2])
@@ -47,9 +76,18 @@ def roster():
         return jsonify(sample_roster)
     return jsonify([])
 
+
 @app.route('/whoshere', methods=['GET'])
 def whoshere():
     if request.method == 'GET':
         return jsonify(sample_students)
 
+    return jsonify([])
+
+
+@app.route('/student', methods=['GET'])
+def student():
+    if request.method == 'GET':
+        student_key = request.args.get('student', '')
+        return jsonify('student data')
     return jsonify([])
--- update4/static/app.js
+++ update4/static/app.js
@@ -1,21 +1,26 @@
-$(document).ready(function() {
-  setUpDatepicker();
-});
+const url = 'http://0.0.0.0:5000/';
+
+function start() {
+  setupDatepicker();
+  setupWhosHere();
+};
+
+start();
 
 function getDateString() {
   return $('#datepicker').datepicker('getDate').toJSON().substr(0,10);
 }
 
-function setUpDatepicker() {
+function setupDatepicker() {
   $("#datepicker").datepicker({
-    onSelect: setUpClassButtons
+    onSelect: setupClassButtons
   }).datepicker("setDate", new Date());
 
-  setUpClassButtons();
+  setupClassButtons();
 }
 
-function setUpClassButtons() {
-  $.get('http://0.0.0.0:5000/classes',
+function setupClassButtons() {
+  $.get(url + 'classes',
     {date: getDateString()},
     function(data) {
       const buttonDiv = document.getElementById('classes');
@@ -28,46 +33,74 @@ function setUpClassButtons() {
   );
 }
 
+function setupWhosHere() {
+  const whoshere = document.getElementsByClassName('whois-button')[0];
+
+  whoshere.onclick = function() {
+    $.get(url + 'whoshere',
+      function(data) {
+        const main = document.getElementById('main');
+        const cont = document.createElement('div');
+        const h1 = document.createElement('h1');
+        h1.innerHTML = "Who's here?";
+        cont.className = 'table-container';
+        main.innerHTML = '';
+        main.appendChild(cont);
+        cont.appendChild(h1);
+        cont.appendChild(createWhosHereTable(data));
+      }
+    );
+  }
+}
+
 function createButton(data) {
   let button = document.createElement('button');
   button.innerHTML = data.name;
   button.className = 'class-button nav-button';
   button.dataset.key = data.key;
-  button.onclick = setUpRoster;
+  button.onclick = setupRoster;
 
   return button;
 }
 
-function setUpRoster() {
+function setupRoster() {
   const date = getDateString();
   const classKey = this.getAttribute("data-key");
-  $.get('http://0.0.0.0:5000/roster',
+  const className = this.innerHTML;
+  $.get(url + 'roster',
     {date: date, class: classKey},
     function(data) {
       const main = document.getElementById('main');
+      const cont = document.createElement('div');
+      const h1 = document.createElement('h1');
+      h1.innerHTML = className;
+      cont.appendChild(h1);
+      cont.className = 'table-container';
       main.innerHTML = '';
-      main.appendChild(createTable(data));
+      main.appendChild(cont);
+      cont.appendChild(createRosterTable(data));
     }
   );
 }
 
-function createTable(data) {
+function createRosterTable(data) {
   const table = document.createElement('table');
+  table.className = 'student-table';
   const headerRow = document.createElement('tr');
-  headerRow.innerHTML = '<td>Student Name</td><td>Sign In</td><td>Sign Out</td>'
+  headerRow.innerHTML = '<th>Student Name</th><th>Sign In</th><th>Sign Out</th>'
   table.appendChild(headerRow);
 
   for (let d of data) {
     let row = document.createElement('tr');
     
     let tdName = document.createElement('td');
-    tdName.innerHTML = d.name;
+    tdName.appendChild(createStudentLink(d));
     
     let tdIn = document.createElement('td');
-    tdIn.innerHTML = d.in;
+    tdIn.appendChild(createCheckbox(d.in, "in", d.key));
 
     let tdOut = document.createElement('td');
-    tdOut.innerHTML = d.out;
+    tdOut.appendChild(createCheckbox(d.out, "out", d.key));
 
     row.appendChild(tdName);
     row.appendChild(tdIn);
@@ -77,4 +110,84 @@ function createTable(data) {
   }
 
   return table;
+}
+
+function createWhosHereTable(data) {
+  const table = document.createElement('table');
+  table.className = 'student-table';
+  const headerRow = document.createElement('tr');
+  headerRow.innerHTML = '<th>Name</th>'
+  table.appendChild(headerRow);
+
+  for (let d of data) {
+    let row = document.createElement('tr');
+    
+    let tdName = document.createElement('td');
+    tdName.appendChild(createStudentLink(d));
+    
+    row.appendChild(tdName);
+    table.appendChild(row);
+  }
+
+  return table;
+}
+
+
+function createStudentLink(data) {
+  const link = document.createElement('a');
+  link.innerHTML = data.name;
+  link.className = 'link';
+  link.dataset.key = data.key;
+  link.onclick = setupStudentPage;
+  return link;
+}
+
+function createCheckbox(checked, inOrOut, key) {
+  const checkId = inOrOut + key;
+  const cont = document.createElement("div");
+  const check = document.createElement("input");
+  const label = document.createElement("label");
+
+  check.type = "checkbox";
+  check.checked = checked;
+  check.className = "sign";
+  check.id = checkId;
+  check.dataset.key = key;
+  
+  if (inOrOut === 'in') {
+    check.onclick = signStudentIn;
+  } else {
+    check.onclick = signStudentOut;
+  }
+
+
+  label.htmlFor = checkId;
+  
+  cont.appendChild(check);
+  cont.appendChild(label);
+
+  return cont;
+}
+
+function setupStudentPage() {
+  const key = this.getAttribute('data-key');
+  $.get(url + 'student',
+    {student: key},
+    function(data) {
+      const main = document.getElementById('main');
+      const cont = document.createElement('div');
+      cont.className = 'main-container';
+      main.innerHTML = '';
+      main.appendChild(cont);
+      cont.innerHTML = data;
+    }
+  );
+}
+
+function signStudentIn() {
+  alert('sign in - ' + this.getAttribute('data-key') + ' - ' + this.checked);
+}
+
+function signStudentOut() {
+  alert('sign out - ' + this.getAttribute('data-key') + ' - ' + this.checked);
 }
