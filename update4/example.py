fa8e06681b4de49fabeec5d52274fe6809c9f12d
Daniel Kerrigan <dkerriga@nd.edu>
2018-04-25 15:15:51 -0400

    frontend js work

--- update4/example.py
+++ update4/example.py
@@ -1,3 +1,4 @@
+import random
 from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
@@ -5,40 +6,67 @@ app = Flask(__name__)
 sample_students = [
     {'name': 'Tanya Williams', 'key': 1},
     {'name': 'Katie Smith', 'key': 2},
-    {'name': 'Bobby Hoover', 'key': 3}
+    {'name': 'Bobby Hoover', 'key': 3},
+    {'name': 'Kelly Fink', 'key': 4},
+    {'name': 'Dian Stilts', 'key': 5},
+    {'name': 'Chelsie Whitely', 'key': 6},
+    {'name': 'Angelo Mendel', 'key': 7},
+    {'name': 'Harvey Mancuso', 'key': 8},
+    {'name': 'Cheri Swanigan', 'key': 9},
+    {'name': 'Rodrick Stilson', 'key': 10},
+    {'name': 'Andre Cornell', 'key': 11},
+    {'name': 'Jordon Holstein', 'key': 12},
+    {'name': 'Julietta Bustos', 'key': 13},
+    {'name': 'Lanette Haskins', 'key': 14},
+    {'name': 'Harry Kindred', 'key': 15},
+    {'name': 'Kari Paine', 'key': 16},
+    {'name': 'Jacquiline Keating', 'key': 17},
+    {'name': 'Star Segawa', 'key': 18},
+    {'name': 'Suanne Desimone', 'key': 19},
+    {'name': 'Kayla Wentzel', 'key': 20},
+    {'name': 'Emmy Spradley', 'key': 21},
+    {'name': 'Marielle Kegley', 'key': 22},
+    {'name': 'Maryalice Detrick', 'key': 23},
+    {'name': 'Kendal Lund', 'key': 24},
+    {'name': 'Luci Trumbull', 'key': 25},
+    {'name': 'Alison Plascencia', 'key': 26},
+    {'name': 'Kimberely Barba', 'key': 27},
+    {'name': 'Maddie Ellinger', 'key': 28},
+    {'name': 'Victor Pujol', 'key': 29},
+    {'name': 'Celesta Jurgens', 'key': 30},
 ]
 
 sample_roster = [
     {'name': 'Tanya Williams', 'key': 1, 'in': True, 'out': True},
-    {'name': 'Katie Smith', 'key': 2, 'in': True, 'out': False},
-    {'name': 'Bobby Hoover', 'key': 3, 'in': False, 'out': False},
-    {'name': 'Tanya Williams', 'key': 4, 'in': True, 'out': True},
-    {'name': 'Katie Smith', 'key': 5, 'in': True, 'out': False},
-    {'name': 'Bobby Hoover', 'key': 6, 'in': False, 'out': False},
-    {'name': 'Tanya Williams', 'key': 7, 'in': True, 'out': True},
-    {'name': 'Katie Smith', 'key': 8, 'in': True, 'out': False},
-    {'name': 'Bobby Hoover', 'key': 9, 'in': False, 'out': False},
-    {'name': 'Tanya Williams', 'key': 10, 'in': True, 'out': True},
-    {'name': 'Katie Smith', 'key': 11, 'in': True, 'out': False},
-    {'name': 'Bobby Hoover', 'key': 12, 'in': False, 'out': False},
-    {'name': 'Tanya Williams', 'key': 13, 'in': True, 'out': True},
-    {'name': 'Katie Smith', 'key': 14, 'in': True, 'out': False},
-    {'name': 'Bobby Hoover', 'key': 15, 'in': False, 'out': False},
-    {'name': 'Tanya Williams', 'key': 16, 'in': True, 'out': True},
-    {'name': 'Katie Smith', 'key': 17, 'in': True, 'out': False},
-    {'name': 'Bobby Hoover', 'key': 18, 'in': False, 'out': False},
-    {'name': 'Tanya Williams', 'key': 19, 'in': True, 'out': True},
-    {'name': 'Katie Smith', 'key': 20, 'in': True, 'out': False},
-    {'name': 'Bobby Hoover', 'key': 21, 'in': False, 'out': False},
-    {'name': 'Tanya Williams', 'key': 22, 'in': True, 'out': True},
-    {'name': 'Katie Smith', 'key': 23, 'in': True, 'out': False},
-    {'name': 'Bobby Hoover', 'key': 24, 'in': False, 'out': False},
-    {'name': 'Tanya Williams', 'key': 25, 'in': True, 'out': True},
-    {'name': 'Katie Smith', 'key': 26, 'in': True, 'out': False},
-    {'name': 'Bobby Hoover', 'key': 27, 'in': False, 'out': False},
-    {'name': 'Tanya Williams', 'key': 28, 'in': True, 'out': True},
-    {'name': 'Katie Smith', 'key': 29, 'in': True, 'out': False},
-    {'name': 'Bobby Hoover', 'key': 30, 'in': False, 'out': False},
+    {'name': 'Katie Smith', 'key': 2, 'in': True, 'out': True},
+    {'name': 'Bobby Hoover', 'key': 3, 'in': True, 'out': True},
+    {'name': 'Kelly Fink', 'key': 4, 'in': True, 'out': False},
+    {'name': 'Dian Stilts', 'key': 5, 'in': True, 'out': True},
+    {'name': 'Chelsie Whitely', 'key': 6, 'in': True, 'out': True},
+    {'name': 'Angelo Mendel', 'key': 7, 'in': True, 'out': True},
+    {'name': 'Harvey Mancuso', 'key': 8, 'in': True, 'out': False},
+    {'name': 'Cheri Swanigan', 'key': 9, 'in': True, 'out': True},
+    {'name': 'Rodrick Stilson', 'key': 10, 'in': True, 'out': True},
+    {'name': 'Andre Cornell', 'key': 11, 'in': True, 'out': True},
+    {'name': 'Jordon Holstein', 'key': 12, 'in': True, 'out': False},
+    {'name': 'Julietta Bustos', 'key': 13, 'in': True, 'out': True},
+    {'name': 'Lanette Haskins', 'key': 14, 'in': True, 'out': True},
+    {'name': 'Harry Kindred', 'key': 15, 'in': True, 'out': True},
+    {'name': 'Kari Paine', 'key': 16, 'in': False, 'out': False},
+    {'name': 'Jacquiline Keating', 'key': 17, 'in': True, 'out': True},
+    {'name': 'Star Segawa', 'key': 18, 'in': False, 'out': False},
+    {'name': 'Suanne Desimone', 'key': 19, 'in': False, 'out': False},
+    {'name': 'Kayla Wentzel', 'key': 20, 'in': True, 'out': True},
+    {'name': 'Emmy Spradley', 'key': 21, 'in': True, 'out': True},
+    {'name': 'Marielle Kegley', 'key': 22, 'in': True, 'out': True},
+    {'name': 'Maryalice Detrick', 'key': 23, 'in': True, 'out': True},
+    {'name': 'Kendal Lund', 'key': 24, 'in': True, 'out': True},
+    {'name': 'Luci Trumbull', 'key': 25, 'in': False, 'out': False},
+    {'name': 'Alison Plascencia', 'key': 26, 'in': True, 'out': False},
+    {'name': 'Kimberely Barba', 'key': 27, 'in': False, 'out': False},
+    {'name': 'Maddie Ellinger', 'key': 28, 'in': True, 'out': True},
+    {'name': 'Victor Pujol', 'key': 29, 'in': True, 'out': True},
+    {'name': 'Celesta Jurgens', 'key': 30, 'in': True, 'out': False},
 ]
 
 sample_classes = [
@@ -49,6 +77,16 @@ sample_classes = [
     {'name': 'Tutoring', 'key': 5}
 ]
 
+kelly = {
+    'name': 'Kelly Fink',
+    'dob': '11-11-10',
+    'classes': ['Brain Health', 'LEGO Robotics', 'Shakespeare', 'Tutoring'],
+    'signout': ['Permission to walk home.', 'Only parents may pick up.'],
+    'emergency': [
+        {'name': 'Harry Fink', 'number': '574-333-4444'},
+        {'name': 'Sheila Fink', 'number': '574-555-5555'}
+        ]
+    }
 
 @app.route('/', methods=['GET'])
 def landing():
@@ -73,14 +111,14 @@ def roster():
     if request.method == 'GET':
         date = request.args.get('date', '')
         class_key = request.args.get('class', '')
-        return jsonify(sample_roster)
+        return jsonify(sorted(sample_roster, key=lambda s: s['name']))
     return jsonify([])
 
 
 @app.route('/whoshere', methods=['GET'])
 def whoshere():
     if request.method == 'GET':
-        return jsonify(sample_students)
+        return jsonify(sorted(sample_students, key=lambda s: s['name']))
 
     return jsonify([])
 
@@ -89,5 +127,12 @@ def whoshere():
 def student():
     if request.method == 'GET':
         student_key = request.args.get('student', '')
-        return jsonify('student data')
+        return jsonify(kelly)
     return jsonify([])
+
+
+@app.route('/rate', methods=['GET'])
+def rate():
+    if request.method == 'GET':
+        return jsonify({'rate': random.randint(70, 100)})
+    return jsonify({'rate': 0.0})
--- update4/static/app.js
+++ update4/static/app.js
@@ -1,17 +1,41 @@
 const url = 'http://0.0.0.0:5000/';
 
 function start() {
-  setupDatepicker();
+  setupDatepickerAndClasses();
   setupWhosHere();
 };
 
 start();
 
-function getDateString() {
-  return $('#datepicker').datepicker('getDate').toJSON().substr(0,10);
+function getDateString(id) {
+  /* return the data as a string from the dakerpicker with the specified id */
+  return $('#' + id).datepicker('getDate').toJSON().substr(0,10);
 }
 
-function setupDatepicker() {
+function createButton(name, key, className, text, clickFcn) {
+  const btn = document.createElement('button');
+  btn.className = className;
+  btn.dataset.key = key;
+  btn.dataset.name = name;
+  btn.innerHTML = text;
+  btn.onclick = clickFcn;
+  return btn;
+}
+
+function createNode(kind, id, className, inner) {
+  const node = document.createElement(kind);
+  if (id) node.id = id;
+  if (className) node.className = className;
+  if (inner) node.innerHTML = inner;
+  return node;
+}
+
+function createDiv(id, className) {
+  return createNode('div', id, className, '');
+}
+
+function setupDatepickerAndClasses() {
+  /* create the classes datepicker and then set up the class buttons */
   $("#datepicker").datepicker({
     onSelect: setupClassButtons
   }).datepicker("setDate", new Date());
@@ -20,20 +44,22 @@ function setupDatepicker() {
 }
 
 function setupClassButtons() {
+  /* use the api to get the classes for the day and create the buttons */
   $.get(url + 'classes',
-    {date: getDateString()},
+    {date: getDateString('datepicker')},
     function(data) {
       const buttonDiv = document.getElementById('classes');
       buttonDiv.innerHTML = '';
 
       for (let d of data) {
-        buttonDiv.appendChild(createButton(d));
+        buttonDiv.appendChild(createButton(d.name, d.key, 'class-button nav-button', d.name, setupRoster));
       }
     }
   );
 }
 
 function setupWhosHere() {
+  /* when the user clicks the who's here button, create the who's here page */
   const whoshere = document.getElementsByClassName('whois-button')[0];
 
   whoshere.onclick = function() {
@@ -53,20 +79,12 @@ function setupWhosHere() {
   }
 }
 
-function createButton(data) {
-  let button = document.createElement('button');
-  button.innerHTML = data.name;
-  button.className = 'class-button nav-button';
-  button.dataset.key = data.key;
-  button.onclick = setupRoster;
-
-  return button;
-}
-
 function setupRoster() {
-  const date = getDateString();
+  /* when a class button is clicked, setup the class roster */
+  const date = getDateString('datepicker');
   const classKey = this.getAttribute("data-key");
-  const className = this.innerHTML;
+  const className = this.getAttribute("data-name");
+  // use the api to get the class info for this day
   $.get(url + 'roster',
     {date: date, class: classKey},
     function(data) {
@@ -74,16 +92,142 @@ function setupRoster() {
       const cont = document.createElement('div');
       const h1 = document.createElement('h1');
       h1.innerHTML = className;
-      cont.appendChild(h1);
       cont.className = 'table-container';
       main.innerHTML = '';
       main.appendChild(cont);
+      cont.appendChild(h1);
+      cont.appendChild(createButton(className, classKey, 'go-to-rate', 'Attendance Rate Info', setupAttendanceRatePage));
       cont.appendChild(createRosterTable(data));
     }
   );
 }
 
+function setupAttendanceRatePage() {
+  /* set up page to allow user to calculate attendance statistics */
+  const name = this.getAttribute('data-name');
+  const key = this.getAttribute('data-key');
+  const main = document.getElementById('main');
+  const cont = document.createElement('div');
+  const form = createAttendanceRateForm(name, key);
+  
+  main.innerHTML = '';
+  cont.className = 'rate-container';
+  
+  main.appendChild(cont);
+  
+  const h1 = document.createElement('h1');
+  h1.id = 'rate-header';
+  h1.innerHTML = name + ' Attendance Rate';
+    
+  const stat = document.createElement('h1');
+  stat.id = 'stat';
+  stat.innerHTML = '';
+  
+  cont.appendChild(h1);
+  cont.appendChild(form);
+  cont.appendChild(stat);
+  cont.appendChild(createButton(name, key, 'back-to-roster', 'Back to Roster', setupRoster));
+  
+  $('#date-start-picker').datepicker().datepicker("setDate", new Date());
+  $('#date-end-picker').datepicker().datepicker("setDate", new Date());
+}
+
+
+function createAttendanceRateForm(name, key) {
+  /* create day of week and date range options for attendance stats */
+  const form = document.createElement('div');
+  form.className = 'rate-form';
+  
+  const daysTitle = document.createElement('div');
+  daysTitle.className = 'days-title';
+  daysTitle.innerHTML = 'Select Days of the Week:';
+  const daysGroup = document.createElement('div');
+  daysGroup.className = 'days';
+  const dayNames = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
+
+  for (let d of dayNames) {
+    const check = document.createElement('input');
+    check.type = "checkbox";
+    check.checked = false;
+    check.id = 'check-' + d;
+    const label = document.createElement('label');
+    label.htmlFor = check.id;
+    label.innerHTML = d;
+
+    daysGroup.appendChild(check);
+    daysGroup.appendChild(label);
+  }
+
+  form.appendChild(daysTitle);
+  form.appendChild(daysGroup);
+  
+  const datesTitle = document.createElement('div');
+  datesTitle.className = 'dates-title';
+  datesTitle.innerHTML = 'Choose Date Range:';
+  const datesPickers = document.createElement('div');
+  datesPickers.className = 'dates-pickers';
+  datesPickers.appendChild(createDatepicker('date-start'));
+  const pTo = document.createElement('div');
+  pTo.id = "dates-to";
+  pTo.innerHTML = 'to';
+  datesPickers.appendChild(pTo);
+  datesPickers.appendChild(createDatepicker('date-end'));
+
+  form.appendChild(datesTitle);
+  form.appendChild(datesPickers);
+
+  form.appendChild(createButton(name, key, 'calc-button', 'Calculate', getAttendanceStatistic));
+
+  return form;
+}
+
+function getAttendanceStatistic() {
+  /* get the attendance rate from the api based on the selections */
+  const statHeader = document.getElementById('stat');
+
+  const days = document.getElementsByClassName('days')[0];
+  var dayStr = '';
+
+  for (let node of days.childNodes) {
+    if (node.nodeName === 'INPUT') {
+      dayStr += node.checked ? '1' : '0';
+    }
+  }
+
+  const data = {
+    start: getDateString('date-start-picker'),
+    end: getDateString('date-end-picker'),
+    class: this.getAttribute('data-key'),
+    days: dayStr
+  };
+
+  $.get(url + 'rate',
+    data,
+    function(response) {
+      statHeader.innerHTML = response.rate + '\%';
+    }
+  );
+}
+
+function createDatepicker(id) {
+  /* create datepicker with given id*/
+  const div = document.createElement('div');
+  div.id = id;
+  const datepicker = document.createElement('input');
+  datepicker.type = 'text';
+  datepicker.id = id + '-picker';
+  const label = document.createElement('label');
+  label.htmlFor = id + '-picker';
+  label.innerHTML = '<i class="material-icons">date_range</i>';
+
+  div.appendChild(datepicker);
+  div.appendChild(label);
+
+  return div;
+}
+
 function createRosterTable(data) {
+  /* create table for class page */
   const table = document.createElement('table');
   table.className = 'student-table';
   const headerRow = document.createElement('tr');
@@ -97,10 +241,10 @@ function createRosterTable(data) {
     tdName.appendChild(createStudentLink(d));
     
     let tdIn = document.createElement('td');
-    tdIn.appendChild(createCheckbox(d.in, "in", d.key));
+    tdIn.appendChild(createRosterCheckbox(d.in, "in", d.key));
 
     let tdOut = document.createElement('td');
-    tdOut.appendChild(createCheckbox(d.out, "out", d.key));
+    tdOut.appendChild(createRosterCheckbox(d.out, "out", d.key));
 
     row.appendChild(tdName);
     row.appendChild(tdIn);
@@ -113,6 +257,7 @@ function createRosterTable(data) {
 }
 
 function createWhosHereTable(data) {
+  /* create table for who's here page */
   const table = document.createElement('table');
   table.className = 'student-table';
   const headerRow = document.createElement('tr');
@@ -132,8 +277,8 @@ function createWhosHereTable(data) {
   return table;
 }
 
-
 function createStudentLink(data) {
+  /* create link to student profile */
   const link = document.createElement('a');
   link.innerHTML = data.name;
   link.className = 'link';
@@ -142,7 +287,8 @@ function createStudentLink(data) {
   return link;
 }
 
-function createCheckbox(checked, inOrOut, key) {
+function createRosterCheckbox(checked, inOrOut, key) {
+  /* create sign in and sign out checkboxes */
   const checkId = inOrOut + key;
   const cont = document.createElement("div");
   const check = document.createElement("input");
@@ -160,7 +306,6 @@ function createCheckbox(checked, inOrOut, key) {
     check.onclick = signStudentOut;
   }
 
-
   label.htmlFor = checkId;
   
   cont.appendChild(check);
@@ -169,25 +314,85 @@ function createCheckbox(checked, inOrOut, key) {
   return cont;
 }
 
+function signStudentIn() {
+  /* use API to update student sign in */
+  console.log('sign in - ' + this.getAttribute('data-key') + ' - ' + this.checked);
+}
+
+function signStudentOut() {
+  /* use API to update student sign out */
+  console.log('sign out - ' + this.getAttribute('data-key') + ' - ' + this.checked);
+}
+
 function setupStudentPage() {
+  /* set up student profile page */
   const key = this.getAttribute('data-key');
   $.get(url + 'student',
     {student: key},
     function(data) {
       const main = document.getElementById('main');
-      const cont = document.createElement('div');
-      cont.className = 'main-container';
       main.innerHTML = '';
-      main.appendChild(cont);
-      cont.innerHTML = data;
+      main.appendChild(createStudentProfile(data));
     }
   );
 }
 
-function signStudentIn() {
-  alert('sign in - ' + this.getAttribute('data-key') + ' - ' + this.checked);
+function createStudentProfile(data) {
+  /* create divs for student profile */
+  const cont = createDiv('', 'student-container');
+
+  cont.appendChild(createBasicInfo(data)); 
+  cont.appendChild(createOtherInfo(data));
+
+  return cont;
 }
 
-function signStudentOut() {
-  alert('sign out - ' + this.getAttribute('data-key') + ' - ' + this.checked);
+function createBasicInfo(data) {
+  /* create image, name, and date of birth */
+  console.log(data);
+
+  const basic = createDiv('', 'basic-info');
+  const nameAge = createDiv('', 'name-age');
+
+  basic.innerHTML = '<img src=' + 'kelly.jpeg' + '>';
+
+  const nameHead = createNode('h1', '', '', data.name);
+  const dob = createNode('p', '', '', 'Date of Birth: ' + data.dob);
+  nameAge.appendChild(nameHead);
+  nameAge.appendChild(dob);
+
+  basic.appendChild(nameAge);
+
+  return basic;
+}
+
+function createOtherInfo(data) {
+  /* create enrolled classes, sign out info, and emergency info */
+  const other = createDiv('', 'other-info');
+  
+  const classes = createDiv('enrolled-classes', 'student-other-card');
+  const signOut = createDiv('sign-out-rules', 'student-other-card');
+  const emergency = createDiv('emergency-info', 'student-other-card');
+
+  const classesHead = createNode('div', '', 'student-other-header', 'Enrolled Classes');
+  const classesInfo = createNode('div', '', 'student-other-info', data.classes.join('<br>'));
+  classes.appendChild(classesHead);
+  classes.appendChild(classesInfo);
+
+  const signoutHead = createNode('div', '', 'student-other-header', 'Sign Out Information');
+  const signoutInfo = createNode('div', '', 'student-other-info', data.signout.join('<br>'));
+  signOut.appendChild(signoutHead);
+  signOut.appendChild(signoutInfo);
+  
+  const emergencyHead = createNode('div', '', 'student-other-header', 'Emergency Contacts');
+  const info = data.emergency.map((d) => d.name + ': ' +  d.number);
+  const emergencyInfo = createNode('div', '', 'student-other-info', info.join('<br>'));
+  emergency.appendChild(emergencyHead);
+  emergency.appendChild(emergencyInfo);
+
+  other.appendChild(emergency);
+  other.appendChild(signOut);
+  other.appendChild(classes);
+
+  return other;
 }
