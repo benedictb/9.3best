b9cffc5215cf002eba020000e663b951cc4229c8
Benedict Becker <becker.benedictj@gmail.com>
2018-05-10 13:15:15 -0400

    uploaded fingerprint scanner demo

---
+++ java/Main.java
@@ -0,0 +1,10 @@
+//package edu.nd.hci;
+
+public class Main {
+
+    public static void main(String[] args) {
+//	    Rollcall rc = new Rollcall("./testPrints");
+        Identification id = new Identification();
+
+    }
+}
